14b6e59dcc2f26f87118e01c340532744f5a80cf
David Greydanus <davidgreydanus@gmail.com>
2015-10-21 20:53:51 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, cord)
+  if (board[cord] == " ") or (board[cord] == "") or not board[cord]
+    return false
+  else
+    return true
+  end
+end
